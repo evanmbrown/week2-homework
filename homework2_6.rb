85e9a7ceb34a298e72bc22ec51a3e6fccfbef043
evanmbrown <evanmbrown84@gmail.com>
2014-09-30 11:31:43 -0400

    Solution to EC #2 added

--- homework2_6.rb
+++ homework2_6.rb
@@ -1,5 +1,2 @@
-#Here's my homework for class 2's exercises
 require 'prime'
-puts "Please enter the hghest value to which you'd like to print primes."
-while
-Prime.take_while {|p| p < (n = gets.chomp.to_i) }
+puts Prime.first(gets.chomp.to_i)
